f05065e3661ae523eb0c4629e0002ab829b3e4f0
Fran <francisco@moka.tv>
2024-03-10 20:08:35 -0300

    dark mode

--- back/app/app/ui/dashboard/sidenav.tsx
+++ back/app/app/ui/dashboard/sidenav.tsx
@@ -8,7 +8,7 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end p-4 md:h-40 justify-center items-center"
+        className="mb-2 flex items-end p-4 md:h-40 justify-center items-center"
         href="/"
       >
         <AcmeLogo />
