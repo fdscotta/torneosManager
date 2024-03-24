0d52105bf5db0c67fc4273bdabe95ce79e122ae1
Fran <francisco@moka.tv>
2024-03-23 22:28:01 -0300

    install vercer adapter

--- front/app/astro.config.mjs
+++ front/app/astro.config.mjs
@@ -1,9 +1,11 @@
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [icon()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel(),
 });
