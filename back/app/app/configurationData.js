6427b573baed5a80a05689073a5d3e7db2e8ee5b
franscottadev <franscotta.dev@gmail.com>
2024-11-06 11:31:49 -0300

    Added new configuration data for `qualificationRoundLeague242526Especial` and updated `tournamentGroupResultActions.ts` to use it for a specific tournament ID.

--- back/app/app/configurationData.js
+++ back/app/app/configurationData.js
@@ -619,6 +619,99 @@ const qualificationRoundTournament242526 = [
     }
 ];
 
+const qualificationRoundLeague242526Especial = [
+    {
+        round: "8",
+        rel_from_1: "1",
+        rel_from_2: "16",
+        rel_to: "81",
+    },
+    {
+        round: "8",
+        rel_from_1: "6",
+        rel_from_2: "11",
+        rel_to: "82",
+    },
+    {
+        round: "8",
+        rel_from_1: "3",
+        rel_from_2: "14",
+        rel_to: "83",
+    },
+    {
+        round: "8",
+        rel_from_1: "8",
+        rel_from_2: "9",
+        rel_to: "84",
+    },
+    {
+        round: "8",
+        rel_from_1: "4",
+        rel_from_2: "13",
+        rel_to: "85",
+    },
+    {
+        round: "8",
+        rel_from_1: "7",
+        rel_from_2: "10",
+        rel_to: "86",
+    },
+    {
+        round: "8",
+        rel_from_1: "2",
+        rel_from_2: "15",
+        rel_to: "87",
+    },
+    {
+        round: "8",
+        rel_from_1: "5",
+        rel_from_2: "12",
+        rel_to: "88",
+    },
+    {
+        round: "4",
+        rel_from_1: "81",
+        rel_from_2: "82",
+        rel_to: "41",
+    },
+    {
+        round: "4",
+        rel_from_1: "83",
+        rel_from_2: "84",
+        rel_to: "42",
+    },
+    {
+        round: "4",
+        rel_from_1: "85",
+        rel_from_2: "86",
+        rel_to: "43",
+    },
+    {
+        round: "4",
+        rel_from_1: "87",
+        rel_from_2: "88",
+        rel_to: "44",
+    },
+    {
+        round: "2",
+        rel_from_1: "41",
+        rel_from_2: "42",
+        rel_to: "21",
+    },
+    {
+        round: "2",
+        rel_from_1: "43",
+        rel_from_2: "44",
+        rel_to: "22",
+    },
+    {
+        round: "1",
+        rel_from_1: "21",
+        rel_from_2: "22",
+        rel_to: "1",
+    }
+];
+
 module.exports = {
     qualificationRoundLeague,
     qualificationRoundLeague242526,
